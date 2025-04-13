6ae40ef5730e0e5edd1b7ee7c3b6d95cb3c71e03
HaMeD RasOOli <rasoolihamed70@gmail.com>
2025-04-13 15:42:06 +0330

    first model and admin panel

--- flashcard/models.py
+++ flashcard/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Flashcard(models.Model):
+    question = models.TextField()
+    answer = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.question[0:10]}..."
