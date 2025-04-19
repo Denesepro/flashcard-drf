d82333b76784602b745623643a828899dd623d69
HaMeD RasOOli <rasoolihamed70@gmail.com>
2025-04-19 19:49:40 +0330

    auth app url and view

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
 
+from authentication.views import CreateUserView
+
 urlpatterns = [
     # Your URLs...
     path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', CreateUserView.as_view(), name='register_user'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from authentication.serializers import CreateUserProfileSerializer
 
 # Create your views here.
+class CreateUserView(APIView):
+    def post(self, request):
+        req_data = request.data
+        serializer = CreateUserProfileSerializer(data=req_data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data
+        user_data = User(
+            username=data['username'],
+            email=data['email'],
+        )
+        user_data.set_password(data['password'])
+        user_data.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
